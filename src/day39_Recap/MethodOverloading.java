d1216d67aca4a63d05778971ea9d159b554f1a7c
burockjohn <brkcn87@gmail.com>
2023-05-06 15:19:05 +0300

    @overriding

---
+++ src/day39_Recap/MethodOverloading.java
@@ -0,0 +1,18 @@
+package day39_Recap;
+
+public class MethodOverloading {
+
+    private void method1() {
+
+
+    }
+
+    private int method1(int a) {
+        return a;
+    }
+
+    public int method1(int a, int b) {
+        return a * b;
+    }
+
+}
