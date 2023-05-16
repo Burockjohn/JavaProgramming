e8aff1568f6250da3338cd643614a3921c9a9c6e
burockjohn <brkcn87@gmail.com>
2023-05-16 10:17:31 +0300

    utilities updated

---
+++ src/utilites/AccessModifiers_Test1.java
@@ -0,0 +1,11 @@
+package utilites;
+
+public class AccessModifiers_Test1 {
+
+    public static void main(String[] args) {
+
+      //  System.out.println(ProtectedAccessModifier.name1); // default is not visible outside the package
+      //  System.out.println(ProtectedAccessModifier.name2); // protected is not always visible outside the package
+
+    }
+}
